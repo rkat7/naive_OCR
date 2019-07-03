43bdc536bbfc5d9a23e60ab45f7219cba0ffd850
Rohith Kattamuri <50071276+rkat7@users.noreply.github.com>
2019-07-03 12:43:59 +0530

    exploring the data

--- data.py
+++ data.py
@@ -1,1 +1,18 @@
+from mnist import MNIST
+from PIL import Image, ImageDraw
 
+# Loading the dataset dataset
+mndata = MNIST('./data/')
+images, labels = mndata.load_training()
+
+# Picking an image from the dataset. Can use a RNG here but picking manually
+i = 4
+image, label = images[i], labels[i]
+
+# Print the image
+output = Image.new("L", (28, 28))
+output.putdata(image)
+output.save("output.png")
+
+# Print label
+print(label)
